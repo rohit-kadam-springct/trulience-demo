0b6a63831692c7faad1f0cef65ad8fbaffdc523f
Rohit kadam <rohitkadamdev@gmail.com>
2025-07-03 11:58:19 +0530

    chore: simple trulience connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,79 @@
-import Image from "next/image";
+"use client";
+
+import { TrulienceAvatar } from "@trulience/react-sdk";
+import { useRef, useState } from "react";
+
+const enum ConnectionState {
+  CONNECTING = 1,
+  CONNECTED = 2,
+  DISCONNECTED = 0
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const avatarRef = useRef<TrulienceAvatar>(null);
+  const [connectionState, setConnectionState] = useState<ConnectionState>(ConnectionState.DISCONNECTED)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const handleOnConnect = () => {
+    const trulienceObj = avatarRef.current?.getTrulienceObject();
+    setConnectionState(ConnectionState.CONNECTING)
+    trulienceObj?.connectGateway();
+  };
+
+  return (
+    <div className="w-screen h-screen relative overflow-hidden">
+      {/* Wrapper to control height responsively */}
+      <div className="w-full h-1/2 md:h-full">
+        <TrulienceAvatar
+          ref={avatarRef}
+          avatarId={"6269824970146662678"}
+          url="https://trulience.com/sdk/trulience.sdk.js"
+          autoConnect={false}
+          prefetchAvatar={true}
+          eventCallbacks={{
+            "websocket-connect": () => {
+              setConnectionState(ConnectionState.CONNECTED)
+            }
+          }}
+          avatarParams={{
+            NativeBar: {
+              enabled: true,
+              style: {
+                bar: {
+                  background: "orange",
+                },
+                container: {
+                  background: "#e0e0de",
+                  "border-radius": "10px",
+                  height: "10px",
+                },
+              },
+            },
+          }}
+        />
+      </div>
+
+      {/* Connect Button */}
+      <button
+        className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded flex items-center gap-2 cursor-pointer
+      absolute bottom-6 left-1/2 transform -translate-x-1/2 z-10"
+        onClick={handleOnConnect}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M3 5l2 2a16 16 0 0014 14l2-2a2 2 0 00-1-3l-4-1a2 2 0 00-2 1l-1 2a12 12 0 01-6-6l2-1a2 2 0 001-2l-1-4a2 2 0 00-3-1z"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </svg>
+        {connectionState === ConnectionState.DISCONNECTED ? "Join Call" : connectionState === ConnectionState.CONNECTING ? "Joining...": "End Call" }
+      </button>
     </div>
   );
 }
